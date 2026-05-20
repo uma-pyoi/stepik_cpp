f43ddcc2c35638458bedcbdcee3a99d53e9ba50b
Assem Yeleussiz <asemeleusiz0022@gmail.com>
2026-05-20 16:25:50 +0100

    (1.8) quadratic equation calculator

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
+#include <cmath>
+
+using namespace std;
 
 int main()
 {
-    char c = '\0';
-    while (std::cin.get(c))
-    { // на каждой итерации считываем один символ в переменную c
-        /* здесь можно пользоваться значением прочитанным в переменную c */
-        if (c != 'a')
-            std::cout << c; // выводим символ, если он не равен 'a'
+    int a, b, c;
+    std::cin >> a >> b >> c;
+
+    double discriminant = (b * b) - (4 * a * c);
+    if (discriminant >= 0.0)
+    {
+        double answer1 = (-b + std::sqrt(discriminant)) / (2 * a);
+        double answer2 = (-b - std::sqrt(discriminant)) / (2 * a);
+        std::cout << answer1 << ' ' << answer2 << '\n';
+    }
+    else
+    {
+        std::cout << "No real roots" << '\n';
     }
 
     return 0;
