9ce2dc4d24912d58c5dedadff93e11853ae7e3ed
Assem Yeleussiz <asemeleusiz0022@gmail.com>
2026-05-25 13:30:10 +0100

    (2.2) call stack and garbage value example :3

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,19 @@
 #include <iostream>
 
-int raise2ToPower(int x)
-{
-    int result = 1;
-    for (int i = 0; i < x; ++i)
-    {
-        result *= 2;
-    }
-    return result;
+void bar() {
+    int c;
+    std::cout << "c: " << c << '\n'; // on the second call in main c will be value of b
+}
+
+void foo() {
+    int b = 67;
+    bar();
 }
 
 int main()
 {
-    int numOfTests{0};
-    std::cin >> numOfTests;
-
-    for (int i = 0; i < numOfTests; ++i)
-    {
-        int a{0};
-        std::cin >> a;
-
-        int biggestPower{0}; // in case when a = 1, so 2 to the power of 0 is 1
-
-        while (true)
-        {
-            int result = raise2ToPower(biggestPower);
-            if (result <= a)
-            {
-                biggestPower++;
-            }
-            else
-            {
-                biggestPower--;
-                break;
-            }
-        }
-        std::cout << biggestPower << '\n';
-    }
+    foo();
+    bar();
 
     return 0;
 }
