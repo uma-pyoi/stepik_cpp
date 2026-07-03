b7b15830f40fa9723501334e1d41c0f9607581ac
Assem Yeleussiz <asemeleusiz0022@gmail.com>
2026-07-03 22:42:36 +0500

    (2.7) transpose 2d matrix

---
+++ src/exercises/resize_dynamic.cpp
@@ -0,0 +1,36 @@
+#if 0
+
+#include <iostream>
+
+char *resize_custom(const char *str, unsigned size, unsigned new_size) {
+    char *new_array = new char[new_size];
+
+    for (int i = 0; i < new_size; ++i) {
+        *(new_array + i) = *(str + i);
+    }
+    delete [] str;
+
+    return new_array;
+}
+
+void print_array(char *array, unsigned size) {
+    for (int i = 0; i < size; ++i) {
+        std::cout << *(array + i) << ' ';
+    }
+    std::cout << '\n';
+}
+
+int main() {
+    unsigned my_size{4};
+    unsigned new_size{3};
+    char *my_array = new char[my_size];
+
+
+    std::cout << "my array: " << '\n';
+    print_array(my_array, my_size);
+
+    char *result_array = resize_custom(my_array, my_size, new_size);
+    print_array(result_array, new_size);
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,66 @@
 #include <iostream>
 
-char *resize_custom(const char *str, unsigned size, unsigned new_size) {
-    char *new_array = new char[new_size];
-
-    for (int i = 0; i < new_size; ++i) {
-        *(new_array + i) = *(str + i);
+void print_2d_matrix(int **m, unsigned rows, unsigned cols) {
+    std::cout << "printing... :3" << '\n';
+    for (int p = 0; p < rows; ++p) {
+        for (int d = 0; d < cols; ++d) {
+            std::cout << m[p][d] << ' ';
+        }
+        std::cout << '\n';
     }
-    delete [] str;
-
-    return new_array;
 }
 
-void print_array(char *array, unsigned size) {
-    for (int i = 0; i < size; ++i) {
-        std::cout << *(array + i) << ' ';
+int **transpose(const int *const *m, unsigned rows, unsigned cols) {
+    int **trans_m = new int *[cols];
+    trans_m[0] = new int[rows * cols];
+    for (int i = 1; i != cols; ++i) {
+        trans_m[i] = trans_m[i - 1] + rows;
     }
-    std::cout << '\n';
+
+    for (int i = 0; i < cols; ++i) {
+        for (int j = 0; j < rows; ++j) {
+            trans_m[i][j] = m[j][i];
+        }
+    }
+
+    return trans_m;
 }
+
 
 int main() {
-    unsigned my_size{4};
-    unsigned new_size{3};
-    char *my_array = new char[my_size];
+    // whatever matrix
+    constexpr unsigned rows{3};
+    constexpr unsigned cols{4};
+    int data[rows][cols] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12}
+    };
+    /*
+     * should become:
+     * 1 5 9
+     * 2 6 10
+     * 3 7 11
+     * 4 8 12
+     *
+     * or
+     *
+     * trans_m[1]: trans_m[3]
+     * trans_m[2]: trans_m[6]
+     * trans_m[3]: trans_m[9]
+     */
+    int *m[rows];
+    for (int i = 0; i < rows; ++i)
+        m[i] = data[i];
 
+    print_2d_matrix(m, rows, cols);
 
-    std::cout << "my array: " << '\n';
-    print_array(my_array, my_size);
+    int **new_m = transpose(m, rows, cols);
 
-    char *result_array = resize_custom(my_array, my_size, new_size);
-    print_array(result_array, new_size);
+    print_2d_matrix(new_m, cols, rows);
+
+    delete[] new_m[0];
+    delete[] new_m;
+
+    return 0;
 }
